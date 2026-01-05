66ff1fd1f3f1f53029575fb6ed9823227ec78119
manikandan130 <manilogu005@gmail.com>
2026-01-06 05:22:39 +0530

    variables updated

--- variables.js
+++ variables.js
@@ -1,6 +1,13 @@
 let a = `mani`;
 let b = 'logu';
 const pi = 3.14
+var name = `can be reassigned`;
+var name = `reassigned again`;
+let name2 = `cannot be reassigned`;
+// name2 = `error`
 
 console.log(a + " "+b);
 alert(pi);
+
+console.log(name);
+console.log(name2);
