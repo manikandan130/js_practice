e205d87540504042e9f6c4fc6c0a845dc5ab2e40
manikandan130 <manilogu005@gmail.com>
2026-01-07 03:53:33 +0530

    learnt data types

---
+++ datatupes.js
@@ -0,0 +1,57 @@
+//primitive datatypes
+
+//number
+let n = 3.30;
+let pi = 3.14;
+console.log(n + " "+pi);
+
+
+//string
+let s = `Hello World`;
+console.log(s);
+
+
+//bool
+let bool = true;
+console.log(bool);
+
+
+
+//undefined
+let unDefined ;
+console.log(unDefined);
+
+
+//null
+let nulle = null;
+console.log(nulle);
+
+// non primitive data types
+
+// object 
+
+let obj =   {
+    name : `mani`,
+    age : 99
+}
+
+console.log(obj);
+
+
+//array
+let arrays1 = [ `BMW`, `BENZ`, `FIAT`];
+
+console.log(arrays1);
+
+
+//FUNCTION
+
+let a = function fun()
+{
+    console.log(`Geeks for geeks`);
+    
+}
+
+console.log("a calling " +a);
+
+
